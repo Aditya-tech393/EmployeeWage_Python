2ef1ffc0718db51ea82b8660b5016947e93d81fc
Aditya-tech393 <adityapd2302@gmail.com>
2025-02-04 15:17:36 +0530

    [Aditya]: Adding part time Employee and wage assuming part time hour is 8hrs

--- EmpWage.py
+++ EmpWage.py
@@ -3,8 +3,13 @@ def check_attendance(attendance):
     if attendance==0:
         return 0
     if attendance==1:
-        print("The wage is: ")
-        return 20*8
+        time=input('Enter type of job: ')
+        if time=='part':
+            print("Your Wage is: ")
+            return 20*6
+        elif time=='full':
+            print("Your Wage is: ")
+            return 20*8
+
 atd_random=random.randint(0,1)
 print(check_attendance(atd_random))
-
